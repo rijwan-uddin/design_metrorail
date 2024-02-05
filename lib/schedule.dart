13f33e5d695803629f45db6be174963e779ee425
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-02-05 23:45:32 +0600

    Schedule file open

---
+++ lib/schedule.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class schedule extends StatefulWidget {
+  const schedule({super.key});
+
+  @override
+  State<schedule> createState() => _scheduleState();
+}
+
+class _scheduleState extends State<schedule> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
