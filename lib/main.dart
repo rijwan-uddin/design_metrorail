125cf8030c8d9eb164334badc406fb3ec5df5a2c
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-02-07 22:14:26 +0600

    positioned widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const metro ='''MRT Line 6 is a  21.26km line extending from Uttara in the north to Kamalapur in the south. The line runs parallel to the Turag River in the west of Dhaka and curves as it moves south-east following the path of the Buriganga River.
+const metro =
+    '''MRT Line 6 is a  21.26km line extending from Uttara in the north to Kamalapur in the south. The line runs parallel to the Turag River in the west of Dhaka and curves as it moves south-east following the path of the Buriganga River.
 The line will be a standard gauge track, including a total of 16 elevated stations crossing busy areas such as Pallabhi and Mirpur. The travel time from north to south Dhaka will be approximately 35 minutes.''';
-
 
 void main() {
   runApp(MyApp());
@@ -47,115 +47,137 @@ class HomePage extends StatelessWidget {
 
   Widget _bodySection() {
     return const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(metro),
-          );
+      padding: EdgeInsets.all(16.0),
+      child: Text(metro),
+    );
   }
 
   Widget _NavSection() {
     return const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.call, color: Colors.blue),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        'CALL',
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  ],
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.call, color: Colors.blue),
+              Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Text(
+                  'CALL',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blue,
+                  ),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.near_me, color: Colors.blue),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        'ROUTE',
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  ],
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.near_me, color: Colors.blue),
+              Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Text(
+                  'ROUTE',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blue,
+                  ),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.share, color: Colors.blue),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        'SHARE',
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  ],
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.share, color: Colors.blue),
+              Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Text(
+                  'SHARE',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blue,
+                  ),
                 ),
-              ],
-            ),
-          );
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _headerSection() {
     return const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Metro Rail',
-                      style:
-                      TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Dhaka, Bangladesh',
-                      style: TextStyle(fontSize: 15, color: Colors.grey),
-                    )
-                  ],
-                ),
-                Spacer(),
-                Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                Text(
-                  '9.5',
-                )
-              ],
-            ),
-          );
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Metro Rail',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Dhaka, Bangladesh',
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              )
+            ],
+          ),
+          Spacer(),
+          Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          Text(
+            '9.5',
+          )
+        ],
+      ),
+    );
   }
 
   Widget _imageSection() {
     return Stack(
       children: [
-        Image.asset('asset/met.jpg',
-                fit: BoxFit.fill,
-                ),
-        Text('WELCOME TO MRT' ,style: TextStyle(fontSize: 16,),)
+        Image.asset(
+          'asset/met.jpg',
+          fit: BoxFit.fill,
+        ),
+        const Positioned(
+          bottom: 10,
+           right: 10,
+            left: 10,
+            child: Text(
+            'WELCOME TO MRT',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.yellow,
+            fontSize: 18,
+          ),
+        )),
+        Positioned(
+          right: 16,
+          bottom: 12,
+          child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.blueAccent,
+                size: 40,
+              )),
+        )
       ],
     );
   }
