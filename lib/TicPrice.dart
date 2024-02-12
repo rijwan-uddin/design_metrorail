41f60f84089a0f79d39ff03ae58ef94eb46f00e8
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-02-12 20:48:05 +0600

    TicketFare update

--- lib/TicPrice.dart
+++ lib/TicPrice.dart
@@ -10,6 +10,152 @@ class TicketPrice extends StatefulWidget {
 class _TicketPriceState extends State<TicketPrice> {
   String? fromValue;
   String? toValue;
+  double fare = 0.0;
+
+  // Map to store fares for each location pair
+  Map<String, Map<String, double>> fares = {
+    'Uttara North': {
+      'Uttara Center': 10.0,
+      'Uttara South': 15.0,
+      'Pallabi': 20.0,
+      'Mirpur11':20,
+      'Mirpur10':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+      // Add fares for other locations
+    },
+    'Uttara Center': {
+      'Uttara North': 10.0,
+      'Uttara South': 15.0,
+      'Pallabi': 20.0,
+      'Mirpur11':20,
+      'Mirpur10':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+      // Add fares for other locations
+    },
+    'Uttara South': {
+      'Uttara North': 10.0,
+      'Uttara Center': 15.0,
+      'Pallabi': 20.0,
+      'Mirpur11':20,
+      'Mirpur10':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+      // Add fares for other locations
+    },
+    'pallabi':{
+      'Uttara North': 10.0,
+      'Uttara Center': 15.0,
+      'Uttara South':30,
+      'Mirpur11':20,
+      'Mirpur10':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+
+
+    },
+    'Mirpur11':{
+      'Uttara North': 10.0,
+      'Uttara Center': 15.0,
+      'Uttara South':30,
+      'Pallabi':20,
+      'Mirpur10':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+
+
+    },
+    'Mirpur10':{
+      'Uttara North': 10.0,
+      'Uttara Center': 15.0,
+      'Uttara South':30,
+      'Pallabi':20,
+      'Mirpur11':20,
+      'Kazipara':30,
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+
+
+    },
+    'Kazipara':{
+      'Uttara North': 10.0,
+      'Uttara Center': 15.0,
+      'Uttara South':30,
+      'Pallabi':20,
+      'Mirpur11':20,
+      'Mirpur10':30,
+      
+      'Shewrapara':30,
+      'Agargaon':40,
+      'Bijoysaraoni':50,
+      'Farmgate':60,
+      'Kawranbazar':70,
+      'Shahbagh':70,
+      'Dhaka University':80,
+      'Sochibaloy':90,
+      'Motijhil':100,
+      'Komlapur':100,
+
+
+    },
+    // Add fares for other locations
+  };
+
+  bool isConfirmationVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +180,7 @@ class _TicketPriceState extends State<TicketPrice> {
       'Sochibaloy',
       'Motijhil',
       'Komlapur',
+      // Add other locations
     ];
 
     return Scaffold(
@@ -56,7 +203,7 @@ class _TicketPriceState extends State<TicketPrice> {
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(1),
+                        color: Colors.grey.withOpacity(0.5),
                         spreadRadius: 3,
                         blurRadius: 7,
                         offset: Offset(0, 3),
@@ -66,7 +213,7 @@ class _TicketPriceState extends State<TicketPrice> {
                   child: Text(
                     'From',
                     style: TextStyle(
-                      fontSize: screenWidth * 0.05, // Adjust font size based on screen width
+                      fontSize: screenWidth * 0.05,
                       color: Colors.white,
                     ),
                   ),
@@ -89,7 +236,7 @@ class _TicketPriceState extends State<TicketPrice> {
                   child: Text(
                     'To',
                     style: TextStyle(
-                      fontSize: screenWidth * 0.05, // Adjust font size based on screen width
+                      fontSize: screenWidth * 0.05,
                       color: Colors.white,
                     ),
                   ),
@@ -105,7 +252,9 @@ class _TicketPriceState extends State<TicketPrice> {
                 DropdownButton<String>(
                   hint: Text('Select from list'),
                   value: fromValue,
-                  items: locations.map((String location) {
+                  items: locations
+                      .where((location) => location != toValue)
+                      .map((String location) {
                     return DropdownMenuItem<String>(
                       value: location,
                       child: Text(location),
@@ -114,14 +263,18 @@ class _TicketPriceState extends State<TicketPrice> {
                   onChanged: (value) {
                     setState(() {
                       fromValue = value;
+                      isConfirmationVisible = false;
                     });
                   },
                 ),
-                SizedBox(width: 20.0),
+                SizedBox(width: 20),
                 DropdownButton<String>(
+                  iconEnabledColor: Colors.green,
                   hint: Text('Select from list'),
                   value: toValue,
-                  items: locations.map((String location) {
+                  items: locations
+                      .where((location) => location != fromValue)
+                      .map((String location) {
                     return DropdownMenuItem<String>(
                       value: location,
                       child: Text(location),
@@ -130,25 +283,37 @@ class _TicketPriceState extends State<TicketPrice> {
                   onChanged: (value) {
                     setState(() {
                       toValue = value;
+                      isConfirmationVisible = false;
                     });
                   },
                 ),
               ],
             ),
           ),
-          const Expanded(
-            child: Center(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Add your other widgets here if needed
-                  ],
-                ),
+          if (fromValue != null && toValue != null)
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  fare = fares[fromValue!]![toValue!]!;
+                  isConfirmationVisible = true;
+                });
+              },
+              child: Text('Confirm'),
+            ),
+          if (isConfirmationVisible)
+            Container(
+
+              margin: EdgeInsets.symmetric(vertical: 20),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.lime,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                'Fare from $fromValue to $toValue: $fare',
+                style: TextStyle(fontSize: 18),
               ),
             ),
-          ),
         ],
       ),
     );
